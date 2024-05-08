187c7d6d60b93c24bc5b37644eb30442764ab445
phoenix1998 <estanivegetti@hotmail.com>
2024-05-08 11:48:05 -0300

    Implementacion de tramite baja

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteBaja.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteBaja.cs
@@ -1,6 +1,7 @@
 ﻿using SGE.Aplicacion.Entidades;
 using SGE.Aplicacion.Enumerativos;
 using SGE.Aplicacion.Interfaces;
+using SGE.Aplicacion.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,36 @@ using System.Threading.Tasks;
 
 namespace SGE.Aplicacion.CasosDeUso
 {
-    public class CasoDeUsoTramiteBaja(ITramiteRepositorio repo)
+    public class CasoDeUsoTramiteBaja(ITramiteRepositorio repo, ServicioActualizacionEstado updater, IExpedienteRepositorio expRepo)
     {
-        public void BajaTramite(int id, int idU, Permiso permiso)
+        bool ok = false; // si no lo encuentra
+        public void BajaTramite(int id, int idU, Permiso permiso, Expediente expediente)
         {
-            repo.BajaTramite(id, idU, permiso);
+            Tramite aux = new Tramite();
+            foreach(Tramite tra in expediente.Tramites) {
+                Console.WriteLine("Entro al foreach");
+                
+                if(tra.IDTramite == id)
+                {
+                    aux = tra;
+                    Console.WriteLine("Entro al if");
+
+                    ok = true;
+                    
+                }
+                
+            }
+            if (!ok)
+            {
+                Console.WriteLine("No se encontro el tramite en el expediente");
+            }
+            else
+            {
+                repo.BajaTramite(id, idU, permiso);
+                expediente.Tramites.Remove(aux);
+                updater.ActualizarEstado(expediente);
+                expRepo.ModificacionExpediente(expediente.IDExpediente, expediente, 1, (Permiso)1);
+            }
         }
     }
 }
