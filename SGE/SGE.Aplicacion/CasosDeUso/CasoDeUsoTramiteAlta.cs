910794881de57c5d59b43eedabc95ce85ae66841
phoenix1998 <estanivegetti@hotmail.com>
2024-05-08 11:47:36 -0300

    Implementacion de lista y servicio de actualizacion de estado

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
@@ -1,6 +1,7 @@
 ﻿using SGE.Aplicacion.Entidades;
 using SGE.Aplicacion.Enumerativos;
 using SGE.Aplicacion.Interfaces;
+using SGE.Aplicacion.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,14 @@ using System.Threading.Tasks;
 
 namespace SGE.Aplicacion.CasosDeUso
 {
-    public class CasoDeUsoTramiteAlta(ITramiteRepositorio repo)
+    public class CasoDeUsoTramiteAlta(ITramiteRepositorio repo, IExpedienteRepositorio expRepo, ServicioActualizacionEstado updater)
     {
-        public void Ejercutar(Tramite tramite,int id,Permiso permiso)
+        public void Ejecutar(Tramite tramite,int id,Permiso permiso, Expediente expediente)
         {
             repo.AltaTramite(tramite,id,permiso);
+            expediente.Tramites.Add(tramite);
+            updater.ActualizarEstado(expediente);
+            expRepo.ModificacionExpediente(expediente.IDExpediente, expediente, 1, (Permiso)1);
         }
     }
 }
