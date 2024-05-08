c21d1de9b3f5c64cbc8e1d6da3effb398a59a79f
phoenix1998 <estanivegetti@hotmail.com>
2024-05-08 11:48:55 -0300

    Implementacion de servicio de actualizacion y modificacion de archivo

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteModificacion.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteModificacion.cs
@@ -1,6 +1,7 @@
 ﻿using SGE.Aplicacion.Entidades;
 using SGE.Aplicacion.Enumerativos;
 using SGE.Aplicacion.Interfaces;
+using SGE.Aplicacion.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,13 @@ using System.Threading.Tasks;
 
 namespace SGE.Aplicacion.CasosDeUso
 {
-    public class CasoDeUsoTramiteModificacion(ITramiteRepositorio repo)
+    public class CasoDeUsoTramiteModificacion(ITramiteRepositorio repo, ServicioActualizacionEstado updater, IExpedienteRepositorio expRepo)
     {
-        public void Ejecutar(int id, Tramite tramite, int idU, Permiso permiso)
+        public void Ejecutar(int id, Tramite tramite, int idU, Permiso permiso, Expediente expediente)
         {
             repo.ModificarTramite(id, tramite, idU, permiso);
+            updater.ActualizarEstado(expediente);
+            expRepo.ModificacionExpediente(expediente.IDExpediente, expediente, 1, (Permiso)1);
         }
        
     }
