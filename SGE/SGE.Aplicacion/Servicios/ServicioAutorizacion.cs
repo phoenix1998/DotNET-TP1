3f4c8e8b8d52389b7a59151137d233548669d8f9
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-06-10 12:30:46 -0300

    Reimplementacion del servicio de autorizacion

---
+++ SGE/SGE.Aplicacion/Servicios/ServicioAutorizacion.cs
@@ -0,0 +1,19 @@
+﻿using SGE.Aplicacion.Enumerativos;
+using SGE.Aplicacion.Excepciones;
+using SGE.Aplicacion.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGE.Aplicacion.Entidades;
+
+namespace SGE.Aplicacion.Servicios
+{
+    public class ServicioAutorizacion: IServicioAutorizacion
+    {
+        public bool PoseeElPermiso(Usuario usuario, Permiso permiso) => 
+            (usuario?.Permisos?.FindIndex(p => p == permiso) ?? -1) != -1;
+    }
+    
+}
