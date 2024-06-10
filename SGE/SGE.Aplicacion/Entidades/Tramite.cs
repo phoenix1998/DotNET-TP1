fa812418716116937e88c2c6f732b96f0695eb44
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-06-10 12:29:43 -0300

    Agregamos id necesario, y cambio de nombres

--- SGE/SGE.Aplicacion/Entidades/Tramite.cs
+++ SGE/SGE.Aplicacion/Entidades/Tramite.cs
@@ -9,16 +9,16 @@ namespace SGE.Aplicacion.Entidades
 {
     public class Tramite
     {
-        public int IdTramite { get; set; }
+        public int Id { get; set; }
         public EtiquetaTramite EtiquetaTramite { get; set; }
         public string? Contenido { get; set; }
         public DateTime FechaHoraCreacion { get; set; }
         public DateTime FechaHoraMod {  get; set; }
-        public int IdUser { get; set; }
+        public int ExpedienteId { get; set; }
 
         public override string ToString()
         {
-            string info = $"ID Tramite: {IdTramite} \nEtiqueta: {EtiquetaTramite} \nContenido: {Contenido} \nFecha creacion: {FechaHoraCreacion} \nFecha modificacion: {FechaHoraMod}\n";
+            string info = $"ID Tramite: {Id} \nEtiqueta: {EtiquetaTramite} \nContenido: {Contenido} \nFecha creacion: {FechaHoraCreacion} \nFecha modificacion: {FechaHoraMod}\n";
             return info;
         }
     }
--- SGE/SGE.Aplicacion/Entidades/Usuario.cs
+++ SGE/SGE.Aplicacion/Entidades/Usuario.cs
@@ -4,6 +4,7 @@ namespace SGE.Aplicacion.Entidades;
 
 public class Usuario
 {
+    public int Id { get; set; }
     public string? Nombre { get; set; }
     public string? Apellido { get; set; }
     public string? Email { get; set; }
