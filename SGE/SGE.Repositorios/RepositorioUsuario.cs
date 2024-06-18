b11f16a815ad9d8bccc132997d4ff4d207f7c58a
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-06-18 12:43:51 -0300

    Cambios en el codigo

--- SGE/SGE.Repositorios/RepositorioUsuario.cs
+++ SGE/SGE.Repositorios/RepositorioUsuario.cs
@@ -1,16 +1,28 @@
+using Microsoft.EntityFrameworkCore;
 using SGE.Aplicacion.Entidades;
 using SGE.Aplicacion.Enumerativos;
 using SGE.Aplicacion.Excepciones;
 using SGE.Aplicacion.Interfaces;
 
+using SGE.Aplicacion.Servicios;
+
+
 namespace SGE.Repositorios;
 
 public class RepositorioUsuario : Repositorio , IUsuarioRepositorio
 {
+    public IHasher hasher = new Hasher();
     public RepositorioUsuario(SgeContext contexto) : base(contexto) {}
     
     public void AltaUsuario(Usuario usuario)
     {
+        if (usuario.Contraseña == null || usuario.Contraseña == "" || usuario.Nombre == null || usuario.Nombre == "")
+        {
+            throw new RepositorioException("ERROR: El usuario o la contraseña no pueden ser nulos o vacíos");
+        }
+        
+        usuario.Contraseña = hasher.ObtenerHash(usuario.Contraseña);
+        
         Contexto.Usuarios.Add(usuario);
         Contexto.SaveChanges();
     }
@@ -26,5 +38,12 @@ public class RepositorioUsuario : Repositorio , IUsuarioRepositorio
             throw new RepositorioException($"El usuario con id {id} no existe");
         }
         return Contexto.Usuarios.Find(id);
+    }
+    public List<Usuario> ConsultaTodos()
+    {
+        List<Usuario> consultarTodos = Contexto.Usuarios.ToList() ?? new List<Usuario>();
+
+        //Como es una lista, no pasa nada al devolverla vacía
+        return consultarTodos;
     }
 }
