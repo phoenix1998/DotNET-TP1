d4e4ebe380329db4252169a0527a69ac4bb85927
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-06-19 19:54:49 -0300

    implementacion del administrador

--- SGE/SGE.Repositorios/RepositorioUsuario.cs
+++ SGE/SGE.Repositorios/RepositorioUsuario.cs
@@ -25,6 +25,17 @@ public class RepositorioUsuario : Repositorio , IUsuarioRepositorio
         
         Contexto.Usuarios.Add(usuario);
         Contexto.SaveChanges();
+        if (usuario.Id == 1)
+        {
+            usuario.Permisos.Add(Permiso.ExpedienteAlta);
+            usuario.Permisos.Add(Permiso.ExpedienteBaja);
+            usuario.Permisos.Add(Permiso.ExpedienteModificacion);
+            usuario.Permisos.Add(Permiso.TramiteAlta);
+            usuario.Permisos.Add(Permiso.TramiteBaja);
+            usuario.Permisos.Add(Permiso.TramiteModificacion);
+            Contexto.SaveChanges();
+        }
+        
     }
     
     public bool TienePermiso(Usuario usuario, Permiso permiso)
@@ -45,5 +56,31 @@ public class RepositorioUsuario : Repositorio , IUsuarioRepositorio
 
         //Como es una lista, no pasa nada al devolverla vacía
         return consultarTodos;
+    }
+    public void Modificar(Usuario usuario, bool opcion)
+    {
+        /*Usuario? usu;
+        usu = Contexto.Usuarios.Find(usuario);
+        if (usu == null)
+        {
+            throw new RepositorioException("No se encontro el usuario");
+        }
+        usu = usuario;*/
+        if (!opcion)
+        {
+            usuario.Contraseña = hasher.ObtenerHash(usuario.Contraseña);
+        }
+        Contexto.Usuarios.Update(usuario);
+        Contexto.SaveChanges();
+    }
+    public void BajaUsuario(int id)
+    {
+        Usuario? usuario = Contexto.Usuarios.Find(id);
+        if (usuario == null)
+        {
+            throw new RepositorioException($"El usuario con id {id} no existe");
+        }
+        Contexto.Usuarios.Remove(usuario);
+        Contexto.SaveChanges();
     }
 }
